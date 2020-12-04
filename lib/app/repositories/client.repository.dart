3b72c13ed8199e5db500c3e896f05766e348f0de
flavia-almeida <flaviamelo13@hotmail.com>
2020-12-04 16:11:32 -0300

    Adição dos arquivos client. e da pagina de login Falta ajustar o repositorio (Joao API) e sincronizar os dados da pagina de login com o banco de dados.

--- lib/app/repositories/client.repository.dart
+++ lib/app/repositories/client.repository.dart
@@ -1,14 +1,14 @@
 import 'dart:convert';
 
-import 'package:mvc_persistence/app/models/device.model.dart';
+import 'package:mvc_persistence/app/models/client.model.dart';
 import 'package:mvc_persistence/app/settings.dart';
 import 'package:http/http.dart' as http;
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 
-import '../models/device.model.dart';
+import '../models/client.model.dart';
 
-class DeviceRepository {
+class ClientRepository {
   Future<Database> _getDB() async {
     return openDatabase(
       join(await getDatabasesPath(), DATABASE_NAME),
@@ -19,14 +19,14 @@ class DeviceRepository {
     );
   }
 
-  Future create(Device item) async {
+  Future create(Client item) async {
     try {
-      var device = item.toMap();
-      device.remove("IdDevice");
+      var client = item.toMap();
+      client.remove("IdClient");
       final Database db = await _getDB();
       await db.insert(
         TABLE_NAME,
-        device,
+        client,
         conflictAlgorithm: ConflictAlgorithm.replace,
       );
     } catch (e) {
@@ -34,13 +34,13 @@ class DeviceRepository {
     }
   }
 
-Future<List<Device>> getAll() async {
+  Future<List<Client>> getAll() async {
     try {
       var response =
-          await http.get(Uri.encodeFull("https://tapegandofogobicho.azurewebsites.net/api/v1/Devices/4"));
-     if (response.statusCode == 200) {
+      await http.get(Uri.encodeFull("https://tapegandofogobicho.azurewebsites.net/api/v1/Client/"));
+      if (response.statusCode == 200) {
         return (jsonDecode(response.body) as List)
-            .map((x) => Device.fromJson(x))
+            .map((x) => Client.fromJson(x))
             .toList();
       }
 
@@ -48,40 +48,46 @@ Future<List<Device>> getAll() async {
     } catch (e) {
       print("FUDEU");
 
-      return new List<Device>();
+      return new List<Client>();
     }
   }
 
-  Future<Device> read(int id) async {
+  Future<Client> read(int id) async {
     try {
       final Database db = await _getDB();
       final List<Map<String, dynamic>> maps = await db.query(
         TABLE_NAME,
-        where: "IdDevice = ?",
+        where: "IdClient = ?",
         whereArgs: [id],
       );
 
-      return Device(
-        IdDevice: maps[0]['IdDevice'],
+      return Client(
+        Nome : maps[0]['Nome'],
         IdClient: maps[0]['IdClient'],
-        Nick: maps[0]['Nick'],
-        Latitude: maps[0]['Latitude'],
-        Longitude: maps[0]['Longitude'],
+        Email: maps[0]['Email'],
+        Logradouro: maps[0]['Logradouro'],
+        Bairro: maps[0]['Bairro'],
+        Numero: maps[0]['Numero'],
+        Cidade: maps[0]['Cidade'],
+        Estado: maps[0]['Estado'],
+        Telefone: maps[0]['Telefone'],
+        CpfCnpj: maps[0]['CpfCnpj'],
+        Senha: maps[0]['Senha'],
       );
     } catch (ex) {
       print(ex);
-      return new Device();
+      return new Client();
     }
   }
 
-  Future update(Device item) async {
+  Future update(Client item) async {
     try {
       final Database db = await _getDB();
       await db.update(
         TABLE_NAME,
         item.toMap(),
-        where: "IdDevice = ?",
-        whereArgs: [item.IdDevice],
+        where: "IdClient = ?",
+        whereArgs: [item.IdClient],
       );
     } catch (e) {
       print(e);
@@ -94,7 +100,7 @@ Future<List<Device>> getAll() async {
       final Database db = await _getDB();
       await db.delete(
         TABLE_NAME,
-        where: "IdDevice = ?",
+        where: "IdClient = ?",
         whereArgs: [id],
       );
     } catch (e) {
